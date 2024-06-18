a6d297e5943e43fd9337919d6208ab9c82a703d2
Bram Ogus <ogus.bram@gmail.com>
2024-06-18 17:57:01 -0400

    Added Call Lock

--- src/HDF.zig
+++ src/HDF.zig
@@ -4,6 +4,7 @@ const current_version: std.SemanticVersion = .{.major = 1, .minor = 14, .patch =
 
 const HDF = @This();
 
+const call_lock = std.Thread.Mutex{};
 const c = @cImport({
     @cInclude("hdf5.h");
 });
@@ -18,23 +19,27 @@ pub const H5Error = error{
 };
 
 pub fn open() !void {
+    call_lock.lock();
     const herr = c.H5open();
-
+    call_lock.unlock();
     if (herr < 0) {
         return H5Error.InitializationError;
     }
 }
 
 pub fn close() !void {
+    call_lock.lock();
     const herr = c.H5close();
-
+    call_lock.unlock();
     if (herr < 0) {
         return H5Error.DestructionError;
     }
 }
 
 pub fn garbageCollect() !void {
+    call_lock.lock();
     const herr = c.H5garbage_collect();
+    call_lock.unlock();
 
     if (herr < 0) {
         return H5Error.GarbageCollectionError;
@@ -45,7 +50,10 @@ pub fn getLibVersion() !std.SemanticVersion {
     var major: c_uint = undefined;
     var minor: c_uint = undefined;
     var patch: c_uint = undefined;
+
+    call_lock.lock();
     const herr = c.H5get_libversion(&major, &minor, &patch);
+    call_lock.unlock();
 
     if (herr < 0) {
         return H5Error.GenericError;
@@ -57,7 +65,9 @@ pub fn getLibVersion() !std.SemanticVersion {
 }
 
 pub fn freeMemory(buf: *anyopaque) !void {
+    call_lock.lock();
     const herr = c.H5free_memory(buf);
+    call_lock.unlock();
 
     if (herr < 0) {
         return H5Error.GenericError;
@@ -66,7 +76,9 @@ pub fn freeMemory(buf: *anyopaque) !void {
 
 pub fn isLibraryThreadSafe() !bool {
     var is_safe: bool = undefined;
+    call_lock.lock();
     const herr = c.H5is_library_threadsafe(&is_safe);
+    call_lock.unlock();
 
     if (herr < 0) {
         return H5Error.GenericError;
@@ -76,8 +88,9 @@ pub fn isLibraryThreadSafe() !bool {
 }
 
 pub fn setFreeLimits(lims: H5Limits) !void {
+    call_lock.lock();
     const herr = c.H5set_free_list_limits(lims.regular_limit_global, lims.regular_limit_list, lims.arr_limit_global, lims.arr_limit_list, lims.block_limit_global, lims.block_limit_list);
-
+    call_lock.unlock();
     if (herr < 0) {
         return H5Error.GenericError;
     }
--- src/HDF/A.zig
+++ src/HDF/A.zig
@@ -1,10 +1,29 @@
 const std = @import("std");
-const c = @import("../HDF.zig").c;
-const H5Error = @import("../HDF.zig").H5Error;
+const HDF = @import("../HDF.zig");
+const c = HDF.c;
+const H5Error = HDF.H5Error;
+
+const call_lock = HDF.call_lock;
+
+pub const H5Index = enum(u8) {
+    IndexUnknown = c.H5_INDEX_UNKNOWN,
+    IndexName = c.H5_INDEX_NAME,
+    CreationOrder = c.H5_INDEX_CRT_ORDER,
+    Indexes = c.H5_INDEX_N,
+};
+
+pub const H5IterOrder = enum(u8) {
+    IterUnknown = c.H5_ITER_UNKNOWN,
+    Increasing = c.H5_ITER_INC,
+    Decreasing = c.H5_ITER_DEC,
+    Native = c.H5_ITER_NATIVE,
+    IterN = c.H5_ITER_N,
+};
 
 pub export fn open(obj_id: i64, attr_name: *const u8, aapl_id: i64) !i64 {
+    call_lock.lock();
     const attr_id = c.H5Aopen(obj_id, attr_name, aapl_id);
-
+    call_lock.unlock();
     if (attr_id < 0) {
         return H5Error.GenericError;
     }
@@ -13,7 +32,10 @@ pub export fn open(obj_id: i64, attr_name: *const u8, aapl_id: i64) !i64 {
 }
 
 pub export fn close(attr_id: i64) !void {
+
+    call_lock.lock();
     const herr = c.H5Aclose(attr_id);
+    call_lock.unlock();
 
     if (herr < 0) {
         return H5Error.GenericError;
@@ -21,8 +43,9 @@ pub export fn close(attr_id: i64) !void {
 }
 
 pub export fn create(loc_id: i64, attr_name: *const u8, type_id: i64, space_id: i64, acpl_id: i64, aapl_id: i64) !i64 {
+    call_lock.lock();
     const hid = c.H5Acreate2(loc_id, attr_name, type_id, space_id, acpl_id, aapl_id);
-
+    call_lock.unlock();
     if (hid < 0) {
         return H5Error.GenericError;
     }
@@ -31,8 +54,9 @@ pub export fn create(loc_id: i64, attr_name: *const u8, type_id: i64, space_id: 
 }
 
 pub export fn createByName(loc_id: i64, obj_name: *const u8, attr_name: *const u8, type_id: i64, space_id: i64, acpl_id: i64, aapl_id: i64, lapl_id: i64) !i64 {
+    call_lock.lock();
     const hid = c.H5Acreate_by_name(loc_id, obj_name, attr_name, type_id, space_id, acpl_id, aapl_id, lapl_id);
-
+    call_lock.unlock();
     if (hid < 0) {
         return H5Error.GenericError;
     }
@@ -40,4 +64,29 @@ pub export fn createByName(loc_id: i64, obj_name: *const u8, attr_name: *const u
     return hid;
 }
 
-pub export fn delete() !void {}
+pub export fn delete(loc_id: i64, attr_name: *const u8) !void {
+    call_lock.lock();
+    const herr = c.H5Adelete(loc_id, attr_name);
+    call_lock.unlock();
+
+    if (herr < 0) {
+        return H5Error.GenericError;
+    }
+}
+
+pub export fn deleteByIdx(loc_id: i64, obj_name: *const u8, index_type: H5Index, order: H5IterOrder, iterations: u64, lapl_id: i64) !void {
+    const herr = c.H5Adelete_by_idx(loc_id, obj_name, index_type, order, iterations, lapl_id);
+
+    if (herr < 0) {
+        return H5Error.GenericError;
+    }
+}
+
+pub export fn deleteByName(loc_id: i64, obj_name: *const u8, attr_name: *const u8, lapl_id: i64) !void {
+    const herr = c.H5Adelete_by_name(loc_id, obj_name, attr_name, lapl_id);
+
+    if (herr < 0) {
+        return H5Error.GenericError;
+    }
+
+}
